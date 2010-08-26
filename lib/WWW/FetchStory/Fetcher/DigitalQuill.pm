a4bc4797caf5cbd5beeec725b4a2f3f7651e087b
Kathryn Andersen <perlkat@katspace.org>
2010-08-26 12:24:22 +1000

    Another stupid typo.

--- lib/WWW/FetchStory/Fetcher/DigitalQuill.pm
+++ lib/WWW/FetchStory/Fetcher/DigitalQuill.pm
@@ -54,7 +54,7 @@ sub allow {
     my $self = shift;
     my $url = shift;
 
-    return ($url =~ /www\.digitial-quill\.org/);
+    return ($url =~ /www\.digital-quill\.org/);
 } # allow
 
 =head1 Private Methods
