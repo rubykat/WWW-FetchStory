92805da14f8d44f0a18384d586c9ee708f95b8e7
Kathryn Andersen <perlkat@katspace.org>
2023-06-09 19:13:22 +1000

    may this fix the wordcount bug

--- lib/WWW/FetchStory/Fetcher/AO3.pm
+++ lib/WWW/FetchStory/Fetcher/AO3.pm
@@ -137,14 +137,11 @@ sub parse_toc {
     $info{category} = $self->parse_category(%args);
     $info{rating} = $self->parse_rating(%args);
     $info{chapters} = $self->parse_chapter_urls(%args, sid=>$sid);
+    $info{wordcount} = $self->parse_wordcount(%args);
     my $epub_url = $self->parse_epub_url(%args, sid=>$sid);
     if ($epub_url)
     {
         $info{epub_url} = $epub_url;
-    }
-    if ($info{epub_url})
-    {
-        $info{wordcount} = $self->parse_wordcount(%args);
     }
 
     return %info;
